630d8ded131a0c161369b60df871a7289249f290
Vitalijs <klaster83@gmail.com>
2022-06-16 14:42:05 +0300

    Add panel_degradation function

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from datetime import datetime
 
-array2D = []
+INSTALL_DATE = "31.03.2022"
+REDUCTION_COEFFICIENT = 85
 
+
+def panel_degradation(install_date):
+    """Take system install date, calculate timedelta between install date and today
+    and return system theoretical performance after panel degradation"""
+
+    install_date_obj = datetime.strptime(install_date, "%d.%m.%Y")
+    days = (time_now - install_date_obj).days
+    if days < 365:
+        performance = days / 365 * -0.02 + 1
+    else:
+        performance = days / 365 * -0.0055 + 0.9855
+    return round(performance, 4)
+
+
+array2D = []
 filename = "output.txt"
 
 with open(filename, 'r') as f:
     for line in f.readlines():
         array2D.append(line.split(','))
-
-# counter = 1
-# for row in array2D:
-#     if len(row) == 13:
-#         print(f"{counter}) OK")
-#     else:
-#         print(f"{counter}) FAIL!")
-#     counter +=1
 
 time_now = datetime.now()
 month_now = time_now.month
@@ -23,9 +31,10 @@ hour_now = time_now.hour
 print(month_now, hour_now)
 print(type(month_now), type(hour_now))
 
-string_to_print = f"{array2D[hour_now][0]}, {round(int(array2D[hour_now][month_now]) / 85, 2)}kWh"
+string_to_print = f"{array2D[hour_now][0]}, {round(int(array2D[hour_now][month_now]) / REDUCTION_COEFFICIENT, 2)}kWh"
 print(string_to_print)
 
 
-# print(array2D)
-# print(array2D[1][0:2])
+print(panel_degradation(INSTALL_DATE))
+
+
