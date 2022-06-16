eb704ef9f809bf929e77b63a462e92f00c7b3b6b
Vitalijs <klaster83@gmail.com>
2022-06-16 12:01:03 +0300

    Read from output.txt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 array2D = []
 
 filename = "output.txt"
@@ -6,13 +8,23 @@ with open(filename, 'r') as f:
     for line in f.readlines():
         array2D.append(line.split(','))
 
-counter = 1
-for row in array2D:
-    if len(row) == 13:
-        print(f"{counter}) OK")
-    else:
-        print(f"{counter}) FAIL!")
-    counter +=1
+# counter = 1
+# for row in array2D:
+#     if len(row) == 13:
+#         print(f"{counter}) OK")
+#     else:
+#         print(f"{counter}) FAIL!")
+#     counter +=1
+
+time_now = datetime.now()
+month_now = time_now.month
+hour_now = time_now.hour
+
+print(month_now, hour_now)
+print(type(month_now), type(hour_now))
+
+string_to_print = f"{array2D[hour_now][0]}, {round(int(array2D[hour_now][month_now]) / 85, 2)}kWh"
+print(string_to_print)
 
 
 # print(array2D)
